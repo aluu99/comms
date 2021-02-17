3fbcb58fb41f9430106c56a2f757a91559dc1847
Tyler <ty.jbailey32@gmail.com>
2021-02-17 15:44:08 -0800

    test

--- Arduino/my_project/groundstation.ino
+++ Arduino/my_project/groundstation.ino
@@ -12,33 +12,67 @@ const long int SPEED = 9600; // speed in which data will be transferred
 
 long lastSendTime = 0;
 int interval = 10000;
+int counter = 0;
 
 
 RH_RF95 rf95(RFM95_CS, RFM95_IRQ);
 
-
 void setup()
 {
-  Serial.begin(SPEED);
+	Serial.begin(SPEED);
 
-  pinmode(LED, OUTPUT);
-  pinmode(RFM95_RST, OUTPUT);
-  digitalWrite(RFM95_RST, LOW);
+  	pinmode(LED, OUTPUT);
+  	pinmode(RFM95_RST, OUTPUT);
+  	digitalWrite(RFM95_RST, LOW);
 
-  // manual reset
-  digitalWrite(RFM95_RST, HIGH);
-  delay(10);
-  digitalWrite(RFM95_RST, LOW);
-  delay(10);
+  	// manual reset
+  	digitalWrite(RFM95_RST, HIGH);
+  	delay(10);
+  	digitalWrite(RFM95_RST, LOW);
+  	delay(10);
 
-  while(!rf95.init())
-  {
-    if(millis - lastSendTime > interval)
+    while(!rf95.init())
     {
-      Serial.println("RFM95 Radio initialization failed")
-
+		if(counter >= 10){
+			return;
+		}
+      	if(millis() - lastSendTime > interval)
+      	{
+        	Serial.println("RFM95 Radio initialization failed")
+        	lastSendTime = millis;
+        	counter++;
+      	}
     }
+	Serial.println("RFM95 Radio initialization succeeded")
+	lastSendTime = 0;
+	counter = 0;
 
-  }
+	while(!rf95.setFrequency(RFM69_FREQUENCY))
+    {
+		if(counter >= 10){
+			return;
+		}
+      	if(millis() - lastSendTime > interval)
+      	{
+        	Serial.println("RFM95 Frequency setup failed")
+        	lastSendTime = millis;
+        	counter++;
+      	}
+    }
+	Serial.println("RFM95 Frequency	setup succeeded")
+	lastSendTime = 0;
+	counter = 0;
+}
+
+long lastSend = 0;
+int packetNum = 0;
+
+void loop()
+{
+	unsigned char *packet = "Test Message";
+	Serial.print("Sending packet ");
+	Serial.println(packNum);
+
+	rf95.send(packet, strlen(packet));
 
 }
