42dbccd5a45d7e9c5ac1e0fa188a6f014f51366b
Rafael Guevara <rafguevara14@gmail.com>
2021-02-21 12:41:06 -0800

    added code to send messages

--- Arduino/my_project/balloon.ino
+++ Arduino/my_project/balloon.ino
@@ -8,6 +8,9 @@ const int irqPin = 1;
 byte localAddress = 0xAA;
 byte destinationAddress = 0xBB;
 
+long lastSendTime = 0;
+int interval = 200;
+
 //figure out how to say Hello World!
 
 void setup()
@@ -27,7 +30,40 @@ void setup()
 
 void loop(){
 
+  if(millis() - lastSendTime > interval){
+
+    // String sensorData = String(count++);
+
+    sendMessage("Hello World");
+
+    Serial.print("Sending data " + sensorData);
+    Serial.print(" from 0x" + String(localAddress,HEX));
+    Serial.println(" to 0x" + String(destinationAddress,HEX));
 
 
+    lastSendTime = millis();
+
+    interval = random(2000) + 1000;
+
+  } //millis
+
+  //receiving
+
+}//void loop
+
+
+void sendMessage(String outgoing){
+
+  LoRa.beginPacket();
+
+  LoRa.write(destinationAddress);
+
+  LoRa.write(localAddress);
+
+  LoRa.write(outgoing.length());
+
+  LoRa.print(outgoing);
+
+  LoRa.endPacket();
 
 }
